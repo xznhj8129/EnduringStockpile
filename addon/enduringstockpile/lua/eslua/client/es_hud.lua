c84e3ed158a2f46b6908cb9cb7d94fb2de157426
aaaaaaaaaa <7685285+xznhj8129@users.noreply.github.com>
2020-10-09 00:37:34 -0400

    HUD adjustment

--- addon/enduringstockpile/lua/eslua/client/es_hud.lua
+++ addon/enduringstockpile/lua/eslua/client/es_hud.lua
@@ -9,24 +9,27 @@ function PANEL:DoESRads(Rads,xperx)
 	local h=ScrH()
 	local yel=255
 	if xperx=="RADS / MIN" then --fancy color change, ooOOoO
-		yel=10/(math.Clamp(Rads,0,100))*255
+		yel=255-((math.Clamp(Rads,0,50)/50)*255)
 		dcol=Color(255,yel,0,255)
 	else 
 		dcol=Color(0,255,0,255)
 	end
-	if yel<=70 then
-		draw.SimpleText("CAUTION","HudSelectionText",w*0.028,h*0.805,Color(255,0,0,255),0,0) --if there was a font that had a skull and crossbones, i absolutely would have used it here
+	draw.RoundedBox(6,w*0.018,h*0.800,w*0.215,h*0.08,Color(0,0,0,200))
+	if (xperx=="MILIRADS / MIN" and Rads>500) or (xperx=="RADS / MIN" and Rads<50) then
+		draw.SimpleText("CAUTION - HIGH RADIATION LEVELS","HudSelectionText",w*0.028,h*0.860,Color(255,255,0,255),0,0) --if there was a font that had a skull and crossbones, i absolutely would have used it here
+	elseif xperx=="RADS / MIN" and Rads>=50 then
+		draw.SimpleText("DANGER - EXTREME RADIATION LEVELS","HudSelectionText",w*0.028,h*0.860,Color(255,0,0,255),0,0)
 	end
-	draw.RoundedBox(6,w*0.018,h*0.800,180,75,Color(0,0,0,200))
-	draw.SimpleText(Rads,"HudNumbers",w*0.028,h*0.814,dcol,0,0)
+	draw.SimpleText(Rads,"HudNumbers",w*0.028,h*0.816,dcol,0,0)
 	draw.SimpleText(xperx,"HudSelectionText",w*0.028,h*0.845,dcol,0,0)
 end
 
 function PANEL:DoESDosi(Total,Acc,ply)
 	local w=ScrW()
 	local h=ScrH()
-	draw.RoundedBox(6,w*0.13,h*0.800,200,75,Color(0,0,0,200))
-	draw.SimpleText(tostring(math.Round(Acc)).." / "..tostring(math.Round(Total)),"HudNumbers",w*0.133,h*0.814,Color(0,255,0,255),0,0)
+	draw.SimpleText("PERSONAL RADIATION DOSIMETER","HudSelectionText",w*0.028,h*0.805,Color(0,255,0,255),0,0)
+	--draw.RoundedBox(6,w*0.13,h*0.800,200,75,Color(0,0,0,200))
+	draw.SimpleText(tostring(math.Round(Acc)).." / "..tostring(math.Round(Total)),"HudNumbers",w*0.133,h*0.816,Color(0,255,0,255),0,0)
 	draw.SimpleText("DOSE - CURRENT / TOTAL","HudSelectionText",w*0.133,h*0.845,Color(0,255,0,255),0,0)
 
 end
